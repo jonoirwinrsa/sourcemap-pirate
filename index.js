d45426e1f3a36f540f4b4c150440201f59cc76be
Jonathan Irwin <jonoirwinrsa@gmail.com>
2020-02-11 12:53:54 +0200

    fix: actually get it to write files

--- index.js
+++ index.js
@@ -9,21 +9,11 @@ fs.readFile('main.e9423c49.chunk.js.map', (err, rawSourceMap) => {
   sourceMap.SourceMapConsumer.with(parsedMap, null, async consumer => {
 
     consumer.eachMapping(async function (m) {
-
       const content = consumer.sourceContentFor(m.source)
+      await mkdirp(getDirName('./generated/' + m.source))
 
-      await writeFile('./generated/' + m.source, content).then(r => {console.log(r)})
+      fs.writeFileSync('./generated/' + m.source, content)
     })
 
   }).catch(error => console.log(error))
 })
-
-const writeFile = async (path, content) => {
-  await mkdirp(getDirName(path))
-  await fs.writeFileSync(path, content, {
-    encoding: 'utf8',
-    mode: 666,
-    flag: 'w'
-  })
-  return 'success'
-}
